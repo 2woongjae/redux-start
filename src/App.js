d161481cf0350287e75bc66f9dbabc83147d7f45
Mark Lee <mark@protopie.io>
2020-12-28 13:00:55 +0900

    fds17

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { addTodo } from "./actions";
+import { useContext, useEffect, useState } from "react";
+import ReduxContext from "./contexts/ReduxContext";
+import TodoList from "./components/TodoList";
+import Form from "./components/Form";
 
 function App() {
+  const store = useContext(ReduxContext);
+
+  const [state, setState] = useState(store.getState());
+
+  useEffect(() => {
+    const unsubscribe = store.subscribe(() => {
+      setState(store.getState());
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, [store]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <TodoList />
+        <Form />
       </header>
     </div>
   );
+
+  function click() {
+    store.dispatch(addTodo("아무거나"));
+  }
 }
 
 export default App;
