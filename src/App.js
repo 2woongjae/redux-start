2b2b09111ac4299f4cf591c1861a924e3d5c4b94
Mark Lee <mark@protopie.io>
2020-12-29 11:23:41 +0900

    connect1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,8 @@
 import "./App.css";
-import { addTodo } from "./actions";
-import { useContext, useEffect, useState } from "react";
-import ReduxContext from "./contexts/ReduxContext";
 import TodoList from "./components/TodoList";
 import Form from "./components/Form";
 
 function App() {
-  const store = useContext(ReduxContext);
-
-  const [state, setState] = useState(store.getState());
-
-  useEffect(() => {
-    const unsubscribe = store.subscribe(() => {
-      setState(store.getState());
-    });
-    return () => {
-      unsubscribe();
-    };
-  }, [store]);
-
   return (
     <div className="App">
       <header className="App-header">
@@ -27,10 +11,6 @@ function App() {
       </header>
     </div>
   );
-
-  function click() {
-    store.dispatch(addTodo("아무거나"));
-  }
 }
 
 export default App;
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,26 +1,55 @@
-import React, { useContext, useEffect, useState } from "react";
-import { completeTodo } from "../actions";
-import ReduxContext from "../contexts/ReduxContext";
+// import React, { useContext, useEffect, useState } from "react";
+// import { completeTodo } from "../actions";
+// import ReduxContext from "../contexts/ReduxContext";
 
-export default function TodoList() {
-  const store = useContext(ReduxContext);
-  const [todos, setTodos] = useState(store.getState().todos);
+// export default function TodoList() {
+//   const store = useContext(ReduxContext);
+//   const [todos, setTodos] = useState(store.getState().todos);
 
-  useEffect(() => {
-    const unsubscribe = store.subscribe(() => {
-      setTodos(store.getState().todos);
-    });
-    return () => {
-      unsubscribe();
-    };
-  }, [store]);
+//   useEffect(() => {
+//     const unsubscribe = store.subscribe(() => {
+//       setTodos(store.getState().todos);
+//     });
+//     return () => {
+//       unsubscribe();
+//     };
+//   }, [store]);
 
+//   return (
+//     <div>
+//       <ul>
+//         {todos.map((todo, index) => {
+//           function click() {
+//             store.dispatch(completeTodo(index));
+//           }
+//           if (todo.done) {
+//             return (
+//               <li style={{ textDecoration: "line-through" }}>{todo.text}</li>
+//             );
+//           }
+//           return (
+//             <li>
+//               {todo.text} <button onClick={click}>done</button>
+//             </li>
+//           );
+//         })}
+//       </ul>
+//     </div>
+//   );
+// }
+
+import React from "react";
+import { connect } from "react-redux";
+// import { completeTodo } from "../actions";
+
+function TodoList() {
+  const todos = [];
   return (
     <div>
       <ul>
         {todos.map((todo, index) => {
           function click() {
-            store.dispatch(completeTodo(index));
+            // store.dispatch(completeTodo(index));
           }
           if (todo.done) {
             return (
@@ -37,3 +66,13 @@ export default function TodoList() {
     </div>
   );
 }
+
+const mapStateToProps = () => {};
+const mapDispatchToProps = () => {};
+
+const TodoListContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TodoList);
+
+export default TodoListContainer;
